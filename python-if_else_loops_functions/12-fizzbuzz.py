fe031cc8a37a1e9b93deb113e7b9f843acd3da25
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-01-26 02:50:07 -0800

    Fizz Buzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1,100):
+        if i % 15 == 0:
+            i = "FizzBuzz"
+        elif i % 5 == 0:
+            i = "Buzz"
+        elif i % 3 == 0:
+            i = "Buzz"
+        print(i, end=" ")
