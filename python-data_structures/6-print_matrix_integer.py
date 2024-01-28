2841ba3f87b779152acbd6e6435ef9c243019057
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-01-28 02:20:55 -0800

    fixing pycode and try to fix correct output error

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,10 @@ def print_matrix_integer(matrix=[[]]):
     while i < len(matrix):
         j = 0
         while j < len(matrix[0]):
-            print("{:d}".format(matrix[i][j]), end=(" " if j < len(matrix[0]) - 1 else "\n"))
+            print("{:d}".format(matrix[i][j]), end="")
+            if j == (len(matrix[0]) - 1):
+                print()
+            else:
+                print(" ", end="")
             j += 1
         i += 1
