a89fecf45c1ec48374ba8199b101e36c1c45503a
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-01-28 02:24:13 -0800

    try to fix correct output fail 2

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     i = 0
     while i < len(matrix):
         j = 0
-        while j < len(matrix[0]):
+        while j < len(matrix[i]):
             print("{:d}".format(matrix[i][j]), end="")
-            if j == (len(matrix[0]) - 1):
+            if j == (len(matrix[i]) - 1):
                 print()
             else:
                 print(" ", end="")
