b4c3ec544cdd58fd93b1b0ebcc26677d4788f7a7
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-01 10:41:04 -0800

    return count and adding newline

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
             pass
+    print()
+    return count
