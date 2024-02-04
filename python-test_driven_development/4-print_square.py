653082e968d4613da8ae2a22b7529104e8f4bcc5
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-04 10:41:19 -0800

    Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Function that prints a square with the character #
+"""
+
+
+def print_square(size):
+    """
+    Function that prints a square with the character #
+    Return: IDK
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
