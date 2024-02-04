e0625c930fe63f377eebf16d7fc5330c6214e689
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-04 10:53:48 -0800

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Function that prints a text with 2 new lines 
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines 
+    after each of these characters: ., ? and :
+    Return: IDK
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text_list = []
+    single_sentence = ""
+    for letter in text:
+        single_sentence += letter
+        if letter == "." or letter == "?" or letter == ":":
+            text_list.append(single_sentence)
+            single_sentence = ""
+    text_list.append(single_sentence)
+    edited_list = []
+    for txt in text_list:
+        txt = txt.strip()
+        edited_list.append(txt)
+    output = "\n\n".join(edited_list)
+    print(output, end="")
