6b6df7252b7e1d3982af4720f0a8f0ef2b477526
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-04 08:51:42 -0800

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    class Square
+"""
+
+
+class Square:
+    """
+    Private instance attribute: size
+    """
+    def __init__(self, size):
+        self.__size = size
+    pass
