7e4bbc7f4f2036fdb1e170160e041daa68b070ef
Saleh Shahverdiyev <118191764+salehshahverdiyev@users.noreply.github.com>
2024-02-04 22:10:25 +0400

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square
 
 class Square():
     """
-    Documentation for Square class
+    Private instance attribute: size
     """
     def __init__(self, size=0):
         if not isinstance(size, int):
