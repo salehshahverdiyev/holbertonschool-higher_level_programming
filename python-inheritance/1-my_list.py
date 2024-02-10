6b6129c87beb4dadb59c9dfd8678e631bae16b23
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-10 17:32:18 +0400

    My list and its test

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+
+
+class MyList(list):
+    '''
+    class MyList
+    '''
+    def print_sorted(self):
+        '''
+        Method Documentation
+        '''
+        sorted_list = sorted(self)
+        print(sorted_list)
