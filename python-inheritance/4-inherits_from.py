5e6e49ca587e1d943894da80f3f73d363fa5039c
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 14:38:19 +0400

    fixing func

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,11 +8,4 @@ def inherits_from(obj, a_class):
     '''
     Method Documentation
     '''
-    obj_class = type(obj)
-
-    if issubclass(obj_class, a_class):
-        return True
-    for base_class in obj_class.__bases__:
-        if inherits_from(base_class, a_class):
-            return True
-    return False
+    return not type(obj) is a_class and isinstance(obj, a_class)
