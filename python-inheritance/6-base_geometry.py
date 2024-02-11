e222b30a7a79d4a469b22ffbde60033b5fbd1a78
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 15:17:05 +0400

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+
+
+class BaseGeometry:
+    '''
+    class BaseGeometry
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
