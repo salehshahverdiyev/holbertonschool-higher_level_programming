a94e198827e9ecd14ec7b2e3c91252ca6f4d7674
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 23:31:56 +0400

    change func

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,15 +4,19 @@ Module Documentation
 """
 
 
-import json
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    loads = load_from_json_file("add_item.json")
-except Exception:
-    loads = []
+json_name = 'add_item.json'
 
-save_to_json_file(loads + sys.argv[1:], "add_item.json")
+try:
+    my_list = load_from_json_file(json_name)
+except Exception as ex:
+    my_list = []
+for i in sys.argv[1:]:
+    my_list.append(i)
+
+save_to_json_file(my_list, json_name)
