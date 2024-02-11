34b400169c7cce5206734f48bedded12fdb7cb21
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 17:29:44 +0400

    fixing with module importing

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,18 +4,7 @@ Module Documentation
 '''
 
 
-class BaseGeometry:
-    '''
-    class BaseGeometry
-    '''
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -23,12 +12,9 @@ class Rectangle(BaseGeometry):
     class Documentation
     '''
     def __init__(self, width, height):
-        super().__init__()
-
-        self.__width = width
-        self.__height = height
 
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
-print(issubclass(Rectangle, BaseGeometry))
+        self.__width = width
+        self.__height = height
