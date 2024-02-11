346751c34749bdc2c0e4bd90b0932aaf3a99f045
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 23:43:23 +0400

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,11 +6,18 @@ Module Documentation
 
 import json
 import sys
+import os
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    loads = load_from_json_file("add_item.json")
-except Exception:
-    loads = []
-save_to_json_file(loads + sys.argv[1:], "add_item.json")
+filename = "add_item.json"
+
+if os.path.isfile(filename):
+    list_of_args = load_from_json_file(filename)
+else:
+    list_of_args = []
+
+list_of_args.extend(sys.argv[1:])
+save_to_json_file(list_of_args, filename)
