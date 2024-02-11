7498a9aa113c61572df33ad71e43bfe368dda4c0
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 23:35:30 +0400

    writing again from scratch

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,19 +4,19 @@ Module Documentation
 """
 
 
-import sys
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-json_name = 'add_item.json'
-
-try:
-    my_list = load_from_json_file(json_name)
-except Exception as ex:
-    my_list = []
-for i in sys.argv[1:]:
-    my_list.append(i)
-
-save_to_json_file(my_list, json_name)
+def method():
+    """
+    Method Documentation
+    """
+    try:
+        obj = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        obj = []
+    obj += argv[1:]
+    save_to_json_file(obj, "add_item.json")
