d7c12dc91de24cf6a5415bf36f1ce80ac50f3920
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 23:52:53 +0400

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module Documentation
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Method Documentation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method Documentation
+        """
+        new_dict = {}
+        if attrs is not None and all(isinstance(item, str) for item in attrs):
+            for i in attrs:
+                try:
+                    new_dict[i] = self.__dict__[i]
+                except KeyError:
+                    pass
+            return new_dict
+        return self.__dict__
