f4eb993717a7af1856d590088f36dad6c7c3ff71
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 15:39:55 +0400

    fixing pycodestyle erro

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,10 +10,9 @@ class BaseGeometry:
     '''
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         if not type(value) is int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-            
