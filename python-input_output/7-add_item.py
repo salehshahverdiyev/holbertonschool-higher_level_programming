9d19441d35127eb046200517ba2fbcd6f09a1cf8
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-11 23:59:47 +0400

    new version

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,20 +4,21 @@ Module Documentation
 """
 
 
-import json
-import sys
-import os
-
-
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 filename = "add_item.json"
 
-if os.path.isfile(filename):
-    list_of_args = load_from_json_file(filename)
-else:
-    list_of_args = []
+new_list_elements = argv[1:]
 
-list_of_args.extend(sys.argv[1:])
-save_to_json_file(list_of_args, filename)
+try:
+    original_list = load_from_json_file(filename)
+except FileNotFoundError:
+    original_list = []
+
+for item in new_list_elements:
+    original_list.append(item)
+
+save_to_json_file(original_list, filename)
