84c77b977c1edd9a20b8dc01e782495d840e11fe
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-12 00:03:04 +0400

    IDK

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,3 @@
-#!/usr/bin/python3
 """
-Module Documentation
+IDK
 """
-
-
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
-filename = "add_item.json"
-
-new_list_elements = argv[1:]
-
-try:
-    original_list = load_from_json_file(filename)
-except FileNotFoundError:
-    original_list = []
-
-for item in new_list_elements:
-    original_list.append(item)
-
-save_to_json_file(original_list, filename)
