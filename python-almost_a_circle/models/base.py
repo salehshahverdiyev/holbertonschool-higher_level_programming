3d541febf50912e409adbde1d7279f707ca76857
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-13 14:59:15 +0400

    adding readme and models folder

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+'''
+Model Documentation
+'''
+
+
+class Base:
+    '''
+    class Documentation
+    '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''
+        Method Documentation
+        '''
+        if id is not None:
+            self.id = value
+            __nb_objects += 1
+            value = __nb_objects
