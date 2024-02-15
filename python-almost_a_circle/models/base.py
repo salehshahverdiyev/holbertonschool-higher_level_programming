baa75a83e6ab263124aba5c6156cc431a625cfbc
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-15 16:03:52 +0400

    fixing

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,6 +26,6 @@ class Base:
         '''
         Method Documentation.
         '''
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
