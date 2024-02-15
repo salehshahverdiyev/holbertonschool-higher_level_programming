b1fce04153e682cba190030f8f585cf1e478b0c7
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-02-15 15:52:28 +0400

    fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''
-        Method Documentation
+        Method Documentation.
         '''
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
