9f0652dda5f4d4eae145ceef1ee5ec37f28890dc
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-03-24 22:28:02 +0400

    States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- States table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
