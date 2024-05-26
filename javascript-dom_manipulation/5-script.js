2183acac6b09e2c01991df7a264181b74965a342
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-05-26 22:17:20 +0400

    Change the text

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+document.addEventListener("DOMContentLoaded", function() {
+    const updateHeader = document.getElementById("update_header");
+    updateHeader.addEventListener("click", function() {
+      const header = document.querySelector("header");
+      header.textContent = "New Header!!!";
+    });
+});
