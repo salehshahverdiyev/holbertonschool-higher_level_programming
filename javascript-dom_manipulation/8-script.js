bc9317335cbecd380f66180160b40e6a5e7fed84
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-05-26 22:21:06 +0400

    Say Hello!

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+      .then(response => response.json())
+      .then(data => {
+        const helloDiv = document.getElementById("hello");
+        helloDiv.textContent = data.hello;
+      })
+      .catch(error => console.error('Error:', error));
+});
